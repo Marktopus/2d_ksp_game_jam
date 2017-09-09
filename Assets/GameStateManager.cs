5cc66db37a45e014cd5a5289ebda0c80b3b19785
Mark Lauzon <marklauzon@outlook.com>
2017-09-09 13:53:47 -0700

    game state manager

---
+++ Assets/GameStateManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum GameStateType
+{
+  MainMenu,
+  Editor,
+  Game,
+  End
+}
+public class GameStateManager : MonoBehaviour 
+{
+  GameStateType gameState = GameStateType.Editor;
+  List<GameState> states; 
+	void Start () 
+  {
+	}
+	
+	// Update is called once per frame
+	void Update () 
+  {
+    states[(int)gameState].Update();
+	}
+
+  public void ChangeState(GameStateType newState)
+  {
+    states[(int)gameState].End();
+    gameState = newState;
+    states[(int)gameState].Start();
+  }
+}
