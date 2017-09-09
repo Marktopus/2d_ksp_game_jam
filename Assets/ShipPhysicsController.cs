d18305d0cb218a1714457b2afbbb8c98cc2b1618
Cody Thompson <cody.thompson22@gmail.com>
2017-09-09 15:36:12 -0700

    contollers

--- Assets/ShipPhysicsController.cs
+++ Assets/ShipPhysicsController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShipPhysicsContoller : MonoBehaviour {
+public class ShipPhysicsController : MonoBehaviour {
 	private Rigidbody2D parentBody;
 
 	public float throttle = 0;
