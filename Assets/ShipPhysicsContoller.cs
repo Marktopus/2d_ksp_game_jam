e19531a9d10650bbf1fd9ba620d6ad227063e7bc
Cody Thompson <cody.thompson22@gmail.com>
2017-09-09 14:56:46 -0700

    barebones physics controller

---
+++ Assets/ShipPhysicsContoller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipPhysicsContoller : MonoBehaviour {
+	private Rigidbody2D parentBody;
+
+	public float throttle = 0;
+
+	public float wetMass = 1;
+	public float dryMass = 1;
+	public float wetMassConsumptionRate = 0.01f;
+	public float maxForcePerUpate = 1f;
+
+	// Use this for initialization
+	public void Start () {
+		parentBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	public void FixedUpdate () {
+		float massDelt = wetMassConsumptionRate * throttle;
+		float updateForce = maxForcePerUpate * throttle;
+
+		wetMass = Math.Max(0, wetMass - massDelt);
+
+		parentBody.AddRelativeForce(new Vector2(0, updateForce));
+	}
+}
