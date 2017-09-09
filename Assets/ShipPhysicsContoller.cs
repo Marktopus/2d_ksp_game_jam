a64ce1a523f824fed5accf951009cf0b7060ddc9
Cody Thompson <cody.thompson22@gmail.com>
2017-09-09 15:33:58 -0700

    Now with actual mass, and a SetThrottle function

--- Assets/ShipPhysicsContoller.cs
+++ Assets/ShipPhysicsContoller.cs
@@ -8,23 +8,30 @@ public class ShipPhysicsContoller : MonoBehaviour {
 
 	public float throttle = 0;
 
-	public float wetMass = 1;
-	public float dryMass = 1;
-	public float wetMassConsumptionRate = 0.01f;
+	public float wetMass = 0.1f;
+	public float dryMass = 0.1f;
+	public float wetMassConsumptionRate = 0.001f;
 	public float maxForcePerUpate = 1f;
 
 	// Use this for initialization
 	public void Start () {
 		parentBody = GetComponent<Rigidbody2D>();
 	}
-	
+
 	// Update is called once per frame
 	public void FixedUpdate () {
 		float massDelt = wetMassConsumptionRate * throttle;
 		float updateForce = maxForcePerUpate * throttle;
 
 		wetMass = Math.Max(0, wetMass - massDelt);
+		parentBody.mass = wetMass + dryMass;
 
-		parentBody.AddRelativeForce(new Vector2(0, updateForce));
+		if (wetMass > 0) {
+			parentBody.AddRelativeForce(new Vector2(0, updateForce));
+		}
+	}
+
+	public void SetThrottle (float throttle) {
+		this.throttle = throttle;
 	}
 }
