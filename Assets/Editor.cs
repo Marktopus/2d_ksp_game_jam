665202055f5f64be52487d246b015a3a455e854f
Cody Thompson <cody.thompson22@gmail.com>
2017-09-10 13:00:35 -0700

    added an images folder

---
+++ Assets/Editor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Editor {
+	GameObject Player;
+
+	// Use this for initialization
+	public Editor () {
+		Player = GameObject.Find("Player");
+	}
+	
+	public void Start () {
+		Player.transform.position += new Vector3(0, 10, 0);
+	}
+
+	public void End () {
+
+	}
+}
