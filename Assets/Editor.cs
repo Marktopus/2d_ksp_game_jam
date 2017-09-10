6002b97ee90b0399e4ca36d63dfdc50b5f0115d8
Cody Thompson <cody.thompson22@gmail.com>
2017-09-10 16:08:24 -0700

    you can add stuff yo

--- Assets/Editor.cs
+++ Assets/Editor.cs
@@ -9,6 +9,8 @@ public class Editor {
 	GameObject ShipContainer;
 	Dictionary<string, GameObject> PrefabDict;
 
+	GameObject ActivePart;
+
 	public Editor (GameObject shipContainer) {
 		ShipContainer = shipContainer;
 		BuildPrefabDict();
@@ -19,15 +21,25 @@ public class Editor {
 	public void BuildPrefabDict () {
 		PrefabDict = new Dictionary<string, GameObject>();
 		PrefabDict.Add("CapsuleA", Resources.Load("CapsuleAPrefab") as GameObject);
+		PrefabDict.Add("FuelTankA", Resources.Load("FuelTankAPrefab") as GameObject);
+		PrefabDict.Add("EngineA", Resources.Load("EngineAPrefab") as GameObject);
 	}
 
 	public void AddListeners () {
-		(GameObject.Find("CapsuleAButton") as GameObject).GetComponent<Button>().onClick.AddListener(()=>{AddPrefab("CapsuleA");});
+		(GameObject.Find("CapsuleAButton") as GameObject).GetComponent<Button>().onClick.AddListener(() => { AddPart("CapsuleA"); });
+		(GameObject.Find("FuelTankAButton") as GameObject).GetComponent<Button>().onClick.AddListener(() => { AddPart("FuelTankA"); });
+		(GameObject.Find("EngineAButton") as GameObject).GetComponent<Button>().onClick.AddListener(() => { AddPart("EngineA"); });
 	}
 
-	public void AddPrefab (string name) {
+	public void AddPart (string name) {
 		GameObject prefab = PrefabDict[name];
 		GameObject part = GameObject.Instantiate(prefab);
+		if (ActivePart == null) {
+			part.transform.position = new Vector3(0, 20, 0);
+		} else {
+			part.transform.position = ActivePart.transform.position + new Vector3(0, -1, 0);
+		}
+		ActivePart = part;
 		part.transform.parent = ShipContainer.transform;
 	}
 }
